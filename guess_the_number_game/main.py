e1d100892b4745c8bfc21e60d38ed1f9b5fa2144
AndreGrigorian <ahgrigor75@gmail.com>
2024-03-18 15:43:42 -0700

    Added custom mode

--- guess_the_number_game/main.py
+++ guess_the_number_game/main.py
@@ -17,11 +17,16 @@ def play(mode):
         range = 50
     if mode == 3:
         range = 100
+    max_tries = 3
+
+    #Custom mode
+    if mode == 4:
+        range = int(input("Input the maximum value for this game: "))
+        max_tries = int(input("Enter how many tries you want: "))
     
     secret_number = random.randint(1, range)
-    max_tries = 3
     num_of_tries = 0
-    # print(secret_number)    ----uncomment for debugging purposes----
+    # print(secret_number)    #----uncomment for debugging purposes----
     while num_of_tries < max_tries:
         user_guess = int(input(f"Num of tries left: {max_tries - num_of_tries}. Enter your guess between 1 and {range}: "))
         if user_guess == secret_number:
@@ -38,15 +43,15 @@ def play(mode):
 
 
 def main():
-    print("1. Easy\n2. Moderate\n3. Hard\n4. Quit")
+    print("1. Easy\n2. Moderate\n3. Hard\n4. Custom\n5. Quit")
     try: 
         mode = int(input("Select Option: "))
     except:
         print("Enter valid input please")
     
     
-    while mode != 4:
-        if(mode-1 in range(3)):#Ensure user inputs either 1, 2, or 3
+    while mode != 5:
+        if(mode-1 in range(4)):#Ensure user inputs either 1, 2, or 3
             play(mode)
         else:
             print("Not an option")
