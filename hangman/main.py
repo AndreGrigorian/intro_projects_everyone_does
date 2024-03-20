f260b3a8f0c7954dd833d682a07a68e7153fd0de
AndreGrigorian <ahgrigor75@gmail.com>
2024-03-19 19:14:17 -0700

    work in progress. implemented play function and word,txt file

--- hangman/main.py
+++ hangman/main.py
@@ -1,7 +1,28 @@
+"""Simple Hangman console application."""
+
 import random
 
+def play(word):
+    max_tries = 6
+    num_tries = 0
+    guess = input("Guess a letter: ").lower()
+    while(guess != word):
+        num_tries+=1
+        pass
+    print(f"You successfully saved the man! The word was indeed {word}")
+
 def main():
-    pass
+    #parse words.txt file
+    words = []
+    with  open("words.txt", "r") as lines:
+        for line in lines:
+            #remove words that are 2 characters or shorter
+            if(len(line) > 2):
+                words.append(line)
+
+    random_word = random.choice(words).lower()
+    play(random_word)
+
 
 if __name__ == "__main__":
     main()
